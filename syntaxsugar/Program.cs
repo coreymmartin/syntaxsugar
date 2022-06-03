74fbddbc310c2ecff344040f2dcbdb660f3f7fc3
corey <coreymmartin1991@gmail.com>
2022-06-03 10:47:23 -0500

    first commit

---
+++ syntaxsugar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace syntaxsugar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : (answer == 9) ? $"{answer} is equal to nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response);
+        }
+    }
+}
